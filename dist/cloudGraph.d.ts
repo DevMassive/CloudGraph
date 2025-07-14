83793c58c82256234c69535b247dd1901565ebd6
devmassive <massivedeveloper@gmail.com>
2025-07-14 14:58:35 +0900

    Exclude dist/ from master branch and prepare for gh-pages deployment

--- dist/cloudGraph.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Draws a cloud graph on the given canvas with the provided data values.
- * @param {HTMLCanvasElement} canvas - The canvas element to draw on.
- * @param {number[]} values - An array of numerical data points.
- */
-export declare function drawCloudGraph(canvas: HTMLCanvasElement, values: number[]): void;
